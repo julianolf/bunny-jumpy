8400f1eb4b2a4f5a75bf03435c38da639496b4be
Juliano Fernandes <julianofernandes@gmail.com>
2018-11-12 16:51:23 -0200

    move spritesheet manager class to sprite module

---
+++ sprite/spritesheet.py
@@ -0,0 +1,62 @@
+import pygame
+import settings
+from xml.dom import minidom
+
+
+class Spritesheet(object):
+    """Manage image spritesheets."""
+
+    def __init__(self, file_name, color_key=settings.BLACK):
+        """
+        Args:
+            file_name (str): Spritesheet (full path) file name.
+        """
+        super(Spritesheet, self).__init__()
+        self.image = pygame.image.load(file_name).convert()
+        self.info = minidom.parse(file_name.replace('.png', '.xml'))
+        self.color_key = color_key
+
+    def get_info(self, image_name):
+        """Get image position and size.
+
+        Return where in the spritesheet the image starts and what is its size.
+
+        Args:
+            image_name (str): The image name.
+
+        Returns:
+            Four values are returned (x, y, width, height).
+            x (int): X axis value.
+            y (int): Y axis value.
+            width (int): Image size horizontally.
+            height (int): Image size vertically.
+
+        Raises:
+            ValueError: If no entry was found for image_name.
+        """
+        nodes = self.info.getElementsByTagName('SubTexture')
+        for node in nodes:
+            if node.getAttribute('name') == image_name:
+                return map(int, (
+                    node.getAttribute('x'),
+                    node.getAttribute('y'),
+                    node.getAttribute('width'),
+                    node.getAttribute('height')
+                ))
+        raise ValueError(f'{image_name} not found in spritesheet.')
+
+    def get_image(self, image_name):
+        """Get image by name.
+
+        Args:
+            image_name (str): The image name.
+
+        Returns:
+            A pygame.Surface instance representing the image.
+        """
+        x, y, width, height = self.get_info(image_name)
+        image = pygame.Surface((width, height))
+        image.blit(self.image, (0, 0), (x, y, width, height))
+        image = pygame.transform.scale(image, (width // 2, height // 2))
+        image.set_colorkey(self.color_key)
+        return image
